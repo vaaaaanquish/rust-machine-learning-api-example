a84d97dd9ba2c8ef4e9ea880157717978af28de7
6syun9 <6syun9@gmail.com>
2021-09-06 08:38:37 +0900

    add state

--- sample_request.py
+++ sample_request.py
@@ -7,14 +7,19 @@ except:
     print("// -----------------------\n[requirement] pip install requests\n// --------------------------")
     raise
 
+
+def request_image(img, name):
+    print(f'{name} request to localhost:3000')
+    res = requests.post('http://127.0.0.1:3000', data=json.dumps({'img': img, 'name': name}), headers={'content-type': 'application/json'})
+    print('response:', res.text)
+
+
 # get sample image
 print('get sample image from rust-lang.org')
 sample_image_response = requests.get('http://rust-lang.org/logos/rust-logo-128x128-blk.png')
 img = base64.b64encode(sample_image_response.content).decode('utf-8')
 
 # request
-print('sample image request to localhost:3000')
-data = {'img': img}
-headers = {'content-type': 'application/json'}
-res = requests.post('http://127.0.0.1:3000', data=json.dumps(data), headers=headers)
-print('response:', res.text)
+request_image(img, 'request1.png')
+request_image(img, 'request1.png')
+request_image(img, 'request2.png')
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
-use axum::{handler::post, Router, Json};
+use axum::{handler::post, Router, Json, AddExtensionLayer, extract::Extension};
 use serde::{Serialize, Deserialize};
 use serde_json::{json, Value};
 use std::net::SocketAddr;
 use image::imageops::rotate180;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use std::collections::HashSet;
 
 extern crate base64;
 extern crate image;
 
+struct DataState {
+    set: Mutex<HashSet<String>>
+}
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", post(proc));
+    let set = Mutex::new(HashSet::new());
+    let state = Arc::new(DataState { set });
+
+    let app = Router::new()
+        .route("/", post(proc))
+        .layer(AddExtensionLayer::new(state));
+
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     println!("listening on {}", addr);
     axum::Server::bind(&addr)
@@ -20,6 +33,7 @@ async fn main() {
 
 #[derive(Deserialize)]
 struct RequestJson {
+    name: String,
     img: String,
 }
 
@@ -28,10 +42,25 @@ struct ResponseJson {
     result: String,
 }
 
-async fn proc(Json(payload): Json<RequestJson>) -> Json<Value> {
+async fn proc(Json(payload): Json<RequestJson>, Extension(state): Extension<Arc<DataState>>) -> Json<Value> {
     let img_buffer = base64::decode(&payload.img).unwrap();
-    let img = image::load_from_memory(img_buffer.as_slice()).unwrap();
-    let rotate_img = rotate180(&img);
-    rotate_img.save("output.png").unwrap();
-    Json(json!({ "result": "saved output.png" }))
+    let result = check_img(&state, img_buffer, payload.name);
+    Json(json!({ "result": result.await }))
+}
+
+
+async fn check_img(state: &DataState, img_buffer: Vec<u8>, name: String) -> &str {
+    let mut set = state.set.lock().await;
+
+    let result;
+    if set.contains(&name) {
+        result = "skip by duplicated";
+    } else {
+        let img = image::load_from_memory(&img_buffer.as_slice()).unwrap();
+        let rotate_img = rotate180(&img);
+        rotate_img.save(&name).unwrap();
+        set.insert(name);
+        result = "saved output image";
+    }
+    return result
 }
